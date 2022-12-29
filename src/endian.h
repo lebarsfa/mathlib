1f77ffba1ef084a8c2bc5f3e3613838f4e4f444d
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2022-12-29 20:54:44 +0100

    Update endian.h

--- src/endian.h
+++ src/endian.h
@@ -2,24 +2,34 @@
 
 /* From https://stackoverflow.com/questions/8978935/detecting-endianness */
 #if defined(__BYTE_ORDER__)&&(__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+#  ifndef BIG_ENDI
 #  define BIG_ENDI
+#  endif
 #  undef LITTLE_ENDI
 #else
 #ifdef WORDS_BIGENDIAN
+#  ifndef BIG_ENDI
 #  define BIG_ENDI
+#  endif
 #  undef LITTLE_ENDI
 #else
+#  ifndef LITTLE_ENDI
 #  define LITTLE_ENDI
+#  endif
 #  undef BIG_ENDI
 #endif
 #endif
 
 #ifdef BIG_ENDI
+#undef HIGH_HALF
 #define HIGH_HALF 0
+#undef LOW_HALF
 #define  LOW_HALF 1
 #else 
 #ifdef LITTLE_ENDI
+#undef HIGH_HALF
 #define HIGH_HALF 1
+#undef LOW_HALF
 #define  LOW_HALF 0
 #endif
 #endif
